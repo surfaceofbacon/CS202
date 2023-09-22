de2d57a580877671ebe8d5096865067fb17db501
surfaceofbacon <rcindustries17@gmail.com>
2023-09-21 22:08:38 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+import random
+while True:
+    val = random.randint(0,15)
+    print(val, hex(val).strip('0x'))
---
+++ turtleRace.py
@@ -0,0 +1,21 @@
+import random
+import turtle
+turtle.
+turtle.window_height()
+t1 = turtle.Turtle()
+t2 = turtle.Turtle()
+t1.penup()
+t1.goto(0, 100)
+t1.pendown()
+t2.penup()
+t2.goto(0,-100)
+t2.pendown()
+for i in range(101):
+    t1.forward(random.randint(2, 5))
+    t2.forward(random.randint(2, 5))
+if t1.xcor() >= 200:
+    t1.write('Turtle 1 wins!', font=('Arial', 16, 'normal'))
+elif t2.xcor() >= 200:
+    t2.write('Turtle 2 wins!', font=('Arial', 16, 'normal'))
+turtle.done()
+
