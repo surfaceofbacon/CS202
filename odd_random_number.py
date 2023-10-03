11b64a00fd435a4d253f3b843248ccabe203f744
surfaceofbacon <rcindustries17@gmail.com>
2023-10-02 17:51:30 -0700

    recent updates for small projects

--- odd_random_number.py
+++ odd_random_number.py
@@ -1,16 +1,37 @@
 import random
 
 
-def odd_random_number():
-    num = random.randint(25, 129)
+def get_user_input():
+    low = int(input('Please enter your lower integer: '))
+    high = int(input('Please enter your higher integer: '))
+    user_even_odd = int(input('enter a zero for odd, and one for even: '))
+    return high, low, user_even_odd
+
+
+def odd_random_number(lower=1, higher=100):
+    num = random.randint(lower, higher)
     if num % 2 == 1:
         print('your random odd number is', num)
     else:
-        odd_random_number()
+        odd_random_number(lower, higher)
+
+
+def even_random_number(lower=1, higher=100):
+    num = random.randint(lower, higher+1)
+    if num % 2 == 0:
+        print('your random odd number is', num)
+    else:
+        even_random_number(lower, higher)
 
 
 def main():
-    odd_random_number()
+    user_high, user_low, even_odd = get_user_input()
+    if even_odd == 0:
+        odd_random_number(user_low, user_high)
+    elif even_odd == 1:
+        even_random_number(user_low, user_high)
+    else:
+        print('Invalid inputs')
 
 
 if __name__ == '__main__':
