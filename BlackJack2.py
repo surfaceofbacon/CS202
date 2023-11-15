bf89acad0ee435ac62a92c8f7ef3f688830cb5c3
surfaceofbacon <rcindustries17@gmail.com>
2023-11-15 13:36:26 -0800

    first commit

--- BlackJack2.py
+++ BlackJack2.py
@@ -19,6 +19,13 @@ def shuffle_deck():
 play = True
 wins = 0
 losses = 0
+def split():
+    global hand
+    if hand[0] == hand[1]:
+        hand1 = [player_hand[0]]
+        hand2 = [player_hand[1]]
+        hand1.append(deck.pop())
+        hand2.append(deck.pop())
 
 def calculate_hand(hand):
     global have_ace
@@ -39,96 +46,109 @@ def calculate_hand(hand):
 stand = False
 
 work = True
-while play:
-    bet = int(input('What would you like to bet?: '))
-    if bankroll <= 0:
-        print('bankroll balance is zero')
-        work = False
-    else:
-        work = True
-    if bet > bankroll:
-        print('that bet doesnt work')
-        work = False
-    else:
-        work = True
-    have_ace = False
-    dwon = False
-    pwon = False
-    player_hand, dealer_hand, deck = shuffle_deck()
-    if work:
-        print(f'Your Hand: {player_hand[0]}, {player_hand[1]}')
-        print(f'Dealers Hand: {dealer_hand[0]}, (??)')
-    while True and work:
-        handval = calculate_hand(player_hand)
-        if handval == 21:
-            print('You hit 21!')
-            pwon = True
-            break
-        elif handval > 21:
-            if have_ace:
-                handval -= 10
-            else:
-                print('You have busted!')
-                dwon = True
-                break
-        hit_stay = input('Would you like to hit or stay? h/s: ')
-        if hit_stay.lower() == 'h':
-            player_hand.append(deck.pop())
-            if handval < 21:
-                print(player_hand)
-            elif handval == 21:
+def play(hand):
+    global work
+    global bankroll
+    global wins
+    global losses
+    while play:
+        bet = int(input('What would you like to bet?: '))
+        if bankroll <= 0:
+            print('bankroll balance is zero')
+            work = False
+        else:
+            work = True
+        if bet > bankroll:
+            print('that bet doesnt work')
+            work = False
+        else:
+            work = True
+        have_ace = False
+        dwon = False
+        pwon = False
+        if split == 'n':
+            hand, dealer_hand, deck = shuffle_deck()
+        elif split == 'y':
+            dealer_hand, deck = shuffle_deck()
+        if work:
+            print(f'Your Hand: {hand[0]}, {hand[1]}')
+            print(f'Dealers Hand: {dealer_hand[0]}, (??)')
+        double_down = input('Would you like to double down (y/n): ')
+        if double_down.lower() == 'y':
+            hand.append(deck.pop())
+            bet *= 2
+            print(hand)
+        while True and work:
+            handval = calculate_hand(hand)
+            if handval == 21:
                 print('You hit 21!')
                 pwon = True
                 break
-            else:
+            elif handval > 21:
                 if have_ace:
                     handval -= 10
                 else:
-                    print(player_hand)
                     print('You have busted!')
                     dwon = True
                     break
-        elif hit_stay.lower() == 's':
-            break
-    while True and work:
-        stand = False
-        counter = 0
-        print(counter)
-        have_ace = False
-        dhandval = calculate_hand(dealer_hand)
-        if dwon or pwon:
-            break
-        if dhandval > 17:
-            stand = True
-        if dhandval == 21:
-            print(f'dealer hand: {dealer_hand}')
-            print('The Dealer hit 21, you lost')
-            dwon = True
-            break
-        if dhandval > 21:
-            if have_ace:
-                dhandval -= 10
-            else:
-                print(f'dealer hand: {dealer_hand}')
-                print('the dealer busted, you win')
-                pwon = True
+            hit_stay = input('Would you like to hit or stay? h/s: ')
+            if hit_stay.lower() == 'h':
+                hand.append(deck.pop())
+                if handval < 21:
+                    print(hand)
+                elif handval == 21:
+                    print('You hit 21!')
+                    pwon = True
+                    break
+                else:
+                    if have_ace:
+                        handval -= 10
+                    else:
+                        print(hand)
+                        print('You have busted!')
+                        dwon = True
+                        break
+            elif hit_stay.lower() == 's':
                 break
-        if dhandval > handval:
-            print(f'dealer hand: {dealer_hand}')
-            print('the dealer wins')
-            dwon = True
-            break
-        if not stand:
-            dealer_hand.append(deck.pop())
-        counter += 1
-    if dwon:
-        bankroll -= bet
-        losses += 1
-    elif pwon:
-        bankroll += bet*2
-        wins += 1
-    print(f'your current bank balance is {bankroll}')
-    again = input('would you like to play again? y/n: ')
-    if again.lower() == 'n':
-        play = False
-        print(f'Thanks for playing! You had {wins} wins and {losses} losses')
+        while True and work:
+            stand = False
+            counter = 0
+            print(counter)
+            have_ace = False
+            dhandval = calculate_hand(dealer_hand)
+            if dwon or pwon:
+                break
+            if dhandval > 17:
+                stand = True
+            if dhandval == 21:
+                print(f'dealer hand: {dealer_hand}')
+                print('The Dealer hit 21, you lost')
+                dwon = True
+                break
+            if dhandval > 21:
+                if have_ace:
+                    dhandval -= 10
+                else:
+                    print(f'dealer hand: {dealer_hand}')
+                    print('the dealer busted, you win')
+                    pwon = True
+                    break
+            if dhandval > handval:
+                print(f'dealer hand: {dealer_hand}')
+                print('the dealer wins')
+                dwon = True
+                break
+            if not stand:
+                dealer_hand.append(deck.pop())
+            counter += 1
+        if dwon:
+            bankroll -= bet
+            losses += 1
+        elif pwon:
+            bankroll += bet*2
+            wins += 1
+        print(f'your current bank balance is {bankroll}')
+        again = input('would you like to play again? y/n: ')
+        if again.lower() == 'n':
+            play = False
+            print(f'Thanks for playing! You had {wins} wins and {losses} losses')
--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-class MyList:
-    def __int__(self, index, 
+class Counter:
+    def __init__(self, counter, limit):
+        self.counter = counter
+        self.limit = limit
+    def increment(self):
+        if self.counter < limit:
+            self.counter += 1
+    def decrement(self)
+        if self.counter > 0:
+            self.counter -= 1
+    def get_value(self):
+        return self.counter
+
